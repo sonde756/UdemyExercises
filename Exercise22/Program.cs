82fb5bd9a3f944ec3d2025fac781d89ca26bc9d7
sonde756 <edsonrafael756@gmail.com>
2022-12-18 09:53:41 -0300

    new exercise

---
+++ Exercise22/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato
+horas:minutos:segundos.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N.
+Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido. 
+ */
+
+int hora, resto, minutos, segundos; 
+
+int N = int.Parse(Console.ReadLine());
+
+hora = N / 3600;
+resto = N % 3600;
+
+minutos = resto / 60;
+segundos = resto % 60;
+
+Console.WriteLine(hora + ":" + minutos + ":" + segundos);
